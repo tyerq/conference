6a23b16689467e4ac0118070aa53fb69a6bdbec7
Stas Erema <tyerq@ukr.net>
2015-03-22 01:37:08 +0200

    practice 6 create & query confereces

--- src/main/java/com/google/devrel/training/conference/spi/ConferenceApi.java
+++ src/main/java/com/google/devrel/training/conference/spi/ConferenceApi.java
@@ -1,16 +1,21 @@
 package com.google.devrel.training.conference.spi;
 
+import java.util.List;
+
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiMethod.HttpMethod;
 import com.google.api.server.spi.response.UnauthorizedException;
 import com.google.appengine.api.users.User;
 import com.google.devrel.training.conference.Constants;
+import com.google.devrel.training.conference.domain.Conference;
 import com.google.devrel.training.conference.domain.Profile;
+import com.google.devrel.training.conference.form.ConferenceForm;
 import com.google.devrel.training.conference.form.ProfileForm;
 import com.google.devrel.training.conference.form.ProfileForm.TeeShirtSize;
 import com.google.devrel.training.conference.service.OfyService;
 import com.googlecode.objectify.Key;
+import com.googlecode.objectify.cmd.Query;
 
 /**
  * Defines conference APIs.
@@ -86,15 +91,14 @@ public class ConferenceApi {
 		// Create a new Profile entity from the
 		// userId, displayName, mainEmail and teeShirtSize
 		Profile profile = getProfile(user);
-		if (profile == null){
+		if (profile == null) {
 			if (displayName == null)
 				displayName = extractDefaultDisplayNameFromEmail(mainEmail);
 			profile = new Profile(userId, displayName, mainEmail, teeShirtSize);
-		}
-		else
+		} else
 			profile.update(displayName, teeShirtSize);
 
-		// TODO 3 (In Lesson 3)
+		// TODONE 3 (In Lesson 3)
 		// Save the Profile entity in the datastore
 		OfyService.ofy().save().entity(profile).now();
 		// Return the profile
@@ -117,7 +121,7 @@ public class ConferenceApi {
 			throw new UnauthorizedException("Authorization required");
 		}
 
-		// TODO
+		// TODONE
 		// load the Profile Entity
 		String userId = user.getUserId(); // TODONE
 		Key key = Key.create(Profile.class, userId); // TODONE
@@ -128,4 +132,88 @@ public class ConferenceApi {
 		// entity
 		return profile;
 	}
+
+	/**
+	 * Gets the Profile entity for the current user or creates it if it doesn't
+	 * exist
+	 * 
+	 * @param user
+	 * @return user's Profile
+	 */
+	public static Profile getProfileFromUser(User user) {
+		// First fetch the user's Profile from the datastore.
+		Profile profile = ofy().load()
+				.key(Key.create(Profile.class, user.getUserId())).now();
+		if (profile == null) {
+			// Create a new Profile if it doesn't exist.
+			// Use default displayName and teeShirtSize
+			String email = user.getEmail();
+			profile = new Profile(user.getUserId(),
+					extractDefaultDisplayNameFromEmail(email), email,
+					TeeShirtSize.NOT_SPECIFIED);
+		}
+		return profile;
+	}
+
+	/**
+	 * Creates a new Conference object and stores it to the datastore.
+	 *
+	 * @param user
+	 *            A user who invokes this method, null when the user is not
+	 *            signed in.
+	 * @param conferenceForm
+	 *            A ConferenceForm object representing user's inputs.
+	 * @return A newly created Conference Object.
+	 * @throws UnauthorizedException
+	 *             when the user is not signed in.
+	 */
+	@ApiMethod(name = "createConference", path = "conference", httpMethod = HttpMethod.POST)
+	public Conference createConference(final User user,
+			final ConferenceForm conferenceForm) throws UnauthorizedException {
+		if (user == null) {
+			throw new UnauthorizedException("Authorization required");
+		}
+
+		// TODONE (Lesson 4)
+		// Get the userId of the logged in User
+		String userId = user.getUserId();
+
+		// TODONE (Lesson 4)
+		// Get the key for the User's Profile
+		Key<Profile> profileKey = Key.create(Profile.class, userId);
+
+		// TODONE (Lesson 4)
+		// Allocate a key for the conference -- let App Engine allocate the ID
+		// Don't forget to include the parent Profile in the allocated ID
+		final Key<Conference> conferenceKey = factory().allocatedId(profileKey,
+				Conference.class);
+
+		// TODONE (Lesson 4)
+		// Get the Conference Id from the Key
+		final long conferenceId = conferenceKey.getId();
+
+		// TODONE (Lesson 4)
+		// Get the existing Profile entity for the current user if there is one
+		// Otherwise create a new Profile entity with default values
+		Profile profile = getProfileFromUser(user);
+
+		// TODONE (Lesson 4)
+		// Create a new Conference Entity, specifying the user's Profile entity
+		// as the parent of the conference
+		Conference conference = new Conference(conferenceId, userId,
+				conferenceForm);
+
+		// TODONE (Lesson 4)
+		// Save Conference and Profile Entities
+		ofy().save().entities(profile, conference).now();
+
+		return conference;
+	}
+
+	@ApiMethod(name = "queryConferences", path = "queryConferences", httpMethod = HttpMethod.POST)
+	public List<Conference> queryConferences() {
+		Query query = ofy().load().type(Conference.class).order("name");
+		return query.list();
+	}
+
 }
